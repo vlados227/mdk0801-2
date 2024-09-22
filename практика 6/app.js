7b31ea835adc1122e2c382c44d2eec2fadad7375
vlados227 <timcockty@icloud.com>
2024-09-22 16:53:19 +0400

    c

---
+++ практика 6/app.js
@@ -0,0 +1,14 @@
+// let square = () => {
+//     let number = prompt("введите число");
+//     alert(`ваше число: ${number * number}`);
+// }
+// square();
+
+let summ = () => {
+    let first, second;
+    Number(first, second);
+    first = prompt("number 1: ");
+    second = prompt("number 2: ");
+    return first+second;
+}
+document.write(summ());
